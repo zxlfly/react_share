db4b83a5ab6ea9c488b9168ef5f12ffb604fce3a
zxlfly <zxlfly@foxmail.com>
2021-01-13 16:03:34 +0800

    update

--- app/src/components/my-rc-field-form/index.js
+++ app/src/components/my-rc-field-form/index.js
@@ -6,3 +6,6 @@ const Form = _Form;
 Form.useForm = useForm;
 export {Field, useForm};
 export default Form;
+
+// form如果是类组件就不能使用useRef了，得使用createRef，但是ref属性直接放在From组件上的，是拿不到对应的表单实例的，需要穿透一下。
+// 需要使用forwardRef和useImperativeHandle
